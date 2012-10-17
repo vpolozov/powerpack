2ba87c56aa47c4ddfb7c76bd0d4bf18ed946a5b0
rsdpisuy <rsdpisuy@gmail.com>
2012-10-17 11:22:03 +0400

    Just copy local all binaries for tests.

--- workyard/tests/fsyacc/build.bat
+++ workyard/tests/fsyacc/build.bat
@@ -1,3 +1,5 @@
+SET FSP_BASE=%~d0%~p0..\..\..\Debug\bin
+
 setlocal
 if EXIST build.ok DEL /f /q build.ok
 
@@ -9,16 +11,13 @@ if NOT "%FSC:NOTAVAIL=X%" == "%FSC%" (
   goto Skip
 )
 
-SET FSP_BASE=%~d0%~p0..\..\..\Debug\bin
+REM Just copy local all binaries for tests
+xcopy /Y %FSP_BASE%"\*.dll"
+xcopy /Y %FSP_BASE%"\*.exe"
 
-SET FSLEX=%FSP_BASE%"\fslex.exe"
-SET FSYACC=%FSP_BASE%"\fsyacc.exe"
+set fsp_flag=-r FSharp.PowerPack.dll -r FSharp.PowerPack.Compatibility.dll
 
 REM UNICODE test1-unicode
-
-REM Regression test for FSB 1885
-"%FSLEX%" repro1885.fsl
-@if ERRORLEVEL 1 goto Error
 
 "%FSLEX%" --light-off -o test1lex.fs test1lex.fsl
 @if ERRORLEVEL 1 goto Error
@@ -26,19 +25,13 @@ REM Regression test for FSB 1885
 "%FSYACC%" --light-off --module TestParser -o test1.fs test1.fsy
 @if ERRORLEVEL 1 goto Error
 
-"%FSC%" %fsc_flags% -g -o:test1%ILX_SUFFIX%.exe tree.fs test1.fsi test1.fs test1lex.fs main.fs
-@if ERRORLEVEL 1 goto Error
-
-"%PEVERIFY%" test1%ILX_SUFFIX%.exe 
+"%FSC%" %fsc_flags% %fsp_flag% -g -o:test1%ILX_SUFFIX%.exe tree.fs test1.fsi test1.fs test1lex.fs main.fs
 @if ERRORLEVEL 1 goto Error
 
 "%FSYACC%" --light-off --module TestParser -o test2.fs test2.fsy
 @if ERRORLEVEL 1 goto Error
 
-"%FSC%" %fsc_flags% -g -o:test2%ILX_SUFFIX%.exe tree.fs test2.fsi test2.fs test1lex.fs main.fs
-@if ERRORLEVEL 1 goto Error
-
-"%PEVERIFY%" test2%ILX_SUFFIX%.exe
+"%FSC%" %fsc_flags% %fsp_flag% -g -o:test2%ILX_SUFFIX%.exe tree.fs test2.fsi test2.fs test1lex.fs main.fs
 @if ERRORLEVEL 1 goto Error
 
 "%FSLEX%" --light-off --unicode -o test1-unicode-lex.fs test1-unicode-lex.fsl
@@ -47,10 +40,7 @@ REM Regression test for FSB 1885
 "%FSYACC%" --light-off --module TestParser -o test1-unicode.fs test1-unicode.fsy
 @if ERRORLEVEL 1 goto Error
 
-"%FSC%" %fsc_flags% -g -o:test1-unicode%ILX_SUFFIX%.exe tree.fs test1-unicode.fsi test1-unicode.fs test1-unicode-lex.fs main-unicode.fs
-@if ERRORLEVEL 1 goto Error
-
-"%PEVERIFY%" test1-unicode%ILX_SUFFIX%.exe 
+"%FSC%" %fsc_flags% %fsp_flag% -g -o:test1-unicode%ILX_SUFFIX%.exe tree.fs test1-unicode.fsi test1-unicode.fs test1-unicode-lex.fs main-unicode.fs
 @if ERRORLEVEL 1 goto Error
 
 
@@ -61,37 +51,25 @@ REM Regression test for FSB 1885
 "%FSYACC%" --module TestParser -o test1.fs test1.fsy
 @if ERRORLEVEL 1 goto Error
 
-"%FSC%" %fsc_flags% -g -o:test1%ILX_SUFFIX%.exe tree.fs test1.fsi test1.fs test1lex.fs main.fs
+"%FSC%" %fsc_flags% %fsp_flag% -g -o:test1%ILX_SUFFIX%.exe tree.fs test1.fsi test1.fs test1lex.fs main.fs
 @if ERRORLEVEL 1 goto Error
 
-"%PEVERIFY%" test1%ILX_SUFFIX%.exe 
+"%FSYACC%" --module TestParser -o test1compat.fs --ml-compatibility test1.fsy
 @if ERRORLEVEL 1 goto Error
 
-"%FSYACC%" --module TestParser -o test1compat.fs --fs-compatibility test1.fsy
-@if ERRORLEVEL 1 goto Error
-
-"%FSC%" %fsc_flags% -g -o:test1compat%ILX_SUFFIX%.exe tree.fs test1compat.fsi test1compat.fs test1lex.fs main.fs
-@if ERRORLEVEL 1 goto Error
-
-"%PEVERIFY%" test1compat%ILX_SUFFIX%.exe
+"%FSC%" %fsc_flags% %fsp_flag% -g -o:test1compat%ILX_SUFFIX%.exe tree.fs test1compat.fsi test1compat.fs test1lex.fs main.fs
 @if ERRORLEVEL 1 goto Error
 
 "%FSYACC%" --module TestParser -o test2.fs test2.fsy
 @if ERRORLEVEL 1 goto Error
 
-"%FSC%" %fsc_flags% -g -o:test2%ILX_SUFFIX%.exe tree.fs test2.fsi test2.fs test1lex.fs main.fs
+"%FSC%" %fsc_flags% %fsp_flag% -g -o:test2%ILX_SUFFIX%.exe tree.fs test2.fsi test2.fs test1lex.fs main.fs
 @if ERRORLEVEL 1 goto Error
 
-"%PEVERIFY%" test2%ILX_SUFFIX%.exe
+"%FSYACC%" --module TestParser -o test2compat.fs --ml-compatibility test2.fsy
 @if ERRORLEVEL 1 goto Error
 
-"%FSYACC%" --module TestParser -o test2compat.fs --fs-compatibility test2.fsy
-@if ERRORLEVEL 1 goto Error
-
-"%FSC%" %fsc_flags% -g -o:test2compat%ILX_SUFFIX%.exe tree.fs test2compat.fsi test2compat.fs test1lex.fs main.fs
-@if ERRORLEVEL 1 goto Error
-
-"%PEVERIFY%" test2compat%ILX_SUFFIX%.exe
+"%FSC%" %fsc_flags% %fsp_flag% -g -o:test2compat%ILX_SUFFIX%.exe tree.fs test2compat.fsi test2compat.fs test1lex.fs main.fs
 @if ERRORLEVEL 1 goto Error
 
 "%FSLEX%" --unicode -o test1-unicode-lex.fs test1-unicode-lex.fsl
@@ -100,10 +78,7 @@ REM Regression test for FSB 1885
 "%FSYACC%" --module TestParser -o test1-unicode.fs test1-unicode.fsy
 @if ERRORLEVEL 1 goto Error
 
-"%FSC%" %fsc_flags% -g -o:test1-unicode%ILX_SUFFIX%.exe tree.fs test1-unicode.fsi test1-unicode.fs test1-unicode-lex.fs main-unicode.fs
-@if ERRORLEVEL 1 goto Error
-
-"%PEVERIFY%" test1-unicode%ILX_SUFFIX%.exe 
+"%FSC%" %fsc_flags% %fsp_flag% -g -o:test1-unicode%ILX_SUFFIX%.exe tree.fs test1-unicode.fsi test1-unicode.fs test1-unicode-lex.fs main-unicode.fs
 @if ERRORLEVEL 1 goto Error
 
 
